1fa97625d3657cdf5afde2d6a9c031a9bfe0c087
norikenpi <norikenpi@gmail.com>
2024-11-16 00:25:25 +0900

    doc: delete BaseModel docstring.

--- de-langchain/serializable.py
+++ de-langchain/serializable.py
@@ -3,7 +3,14 @@ from typing import Any
 from pydantic import BaseModel
 
 class Serializable(BaseModel, ABC):
+  # Remove default BaseModel init docstrig
   def __init__(self, *args: Any, **kwargs: Any) -> None:
+    """"""
     super().__init__(*args, **kwargs)
+
+  @classmethod
+  def is_lc_serializable(cls) -> bool:
+    # TODO implement to_json method
+    return False
 
 
